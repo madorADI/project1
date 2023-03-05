484d8d4897e7df8677a6f9a3e363416d789a9d02
נויה באור <u9150719@bsmch.net>
2023-03-05 19:34:38 +0200

    feat(LatestAlerts) added alert button

--- frontend/src/components/LatestAlerts.vue
+++ frontend/src/components/LatestAlerts.vue
@@ -1,15 +1,33 @@
 <template>
-    <div>
-    
+  <div>
+    <div class="card latest-alerts" style="width: 18rem">
+      <div class="card-body">
+        <h5 class="card-title">התראות חדשות</h5>
+        <hr>
+        <p class="card-text">
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </p>
+        <a href="#" class="create-alert"><font-awesome-icon icon="fa-solid fa-circle-plus" /></a>
+      </div>
     </div>
-  </template>
-  
-  <script>
-  export default {
-    name: "LatestAlerts"
-  };
-  </script>
-  
-  <style scoped>
-  </style>
-  
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LatestAlerts",
+};
+</script>
+
+<style scoped>
+.latest-alerts {
+  background-color: midnightblue;
+  color: white;
+  text-align: center;
+}
+
+.create-alert {
+  color: white;
+}
+</style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,6 +4,21 @@ import { BootstrapVue } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* import specific icons */
+import { faCirclePlus, faUserSecret } from '@fortawesome/free-solid-svg-icons'
+
+/* add icons to the library */
+library.add(faUserSecret, faCirclePlus)
+
+/* add font awesome icon component */
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
 
 Vue.config.productionTip = false
 
