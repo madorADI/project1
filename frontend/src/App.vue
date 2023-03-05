dc290210ed4cd96cb4cdb248c86385f8bea92da3
נויה באור <u9150719@bsmch.net>
2023-03-05 19:16:18 +0200

    add(LatestAlerts) added base component

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,16 @@
 <template>
   <div id="app">
+    <LatestAlerts/>
   </div>
 </template>
 
 <script>
+import LatestAlerts from "./components/LatestAlerts.vue";
 
 export default {
+  components: {
+    LatestAlerts,
+  },
 
 }
 </script>
---
+++ frontend/src/components/LatestAlerts.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+    
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: "LatestAlerts"
+  };
+  </script>
+  
+  <style scoped>
+  </style>
+  
