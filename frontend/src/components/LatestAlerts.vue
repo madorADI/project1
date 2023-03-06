65553eb4353fde4244ede2348d6595b447a0cf4a
קרן בינדרמן <u9088830@bsmch.net>
2023-03-06 10:18:13 +0200

    delete (Alert.vue) deleted Alert component in order to convert to bootstrap vue table in latestAlerts component

--- frontend/src/components/LatestAlerts.vue
+++ frontend/src/components/LatestAlerts.vue
@@ -3,19 +3,18 @@
     <div class="card latest-alerts" style="width: 18rem">
       <div class="card-body">
         <h5 class="card-title">התראות אחרונות</h5>
-        <hr>
-        <p class="card-text">
-          סינון
-        </p>
-        <hr>
-        <p class="card-text-type">
-          סוג האירוע
-        </p>
-        <p class="card-text-date">
-          תאריך, שעה
-        </p>
-        <Alert v-for="alert in alerts" :key="alert.id" :alert="alert"/>
-        <a href="#" class="create-alert"><font-awesome-icon icon="fa-solid fa-circle-plus" /></a>
+        <hr />
+        <p class="card-text-filter">סינון</p>
+        <hr />
+        <b-table
+          class="alertTable"
+          hover
+          :fields="fields"
+          :items="formattedAlerts"
+        ></b-table>
+        <a href="#" class="create-alert"
+          ><font-awesome-icon class="fa-2xl" icon="fa-solid fa-circle-plus"
+        /></a>
       </div>
     </div>
   </div>
@@ -23,34 +22,63 @@
 
 <script>
 import api from "../api/api.js";
-import Alert from "./Alert.vue"
+import Alert from "./Alert.vue";
 export default {
   name: "LatestAlerts",
   components: { Alert },
   data() {
     return {
-      alerts: [{ event_type: "Attack", 
-                time: "1678040638094", 
-                coordinates: [1, 1]}]
-    }
+      fields: [
+        { key: "coordinates", label: "מיקום" },
+        { key: "time", label: "תאריך" },
+        { key: "event_type", label: "סוג האירוע" },
+      ],
+      alerts: [
+        {
+          event_type: "Attack",
+          time: Date.now(),
+          coordinates: [1, 1],
+        },
+      ],
+    };
   },
-
+  // created() {
+  //   this.getAllAlerts();
+  // },
   methods: {
     async getAllAlerts() {
       this.alerts = await (await api.alerts().getAllAlerts()).data;
-    }
+    },
+  },
+  computed: {
+    formattedAlerts() {
+      return this.alerts.map((alert) => {
+        const fixedAlert = { ...alert };
+        fixedAlert.time = new Date(fixedAlert.time).toLocaleString("en-US");
+
+        return fixedAlert;
+      });
+    },
   },
 };
 </script>
 
 <style scoped>
 .latest-alerts {
-  background-color: midnightblue;
-  color: white;
+  background-color: rgb(43, 58, 103);
+  color: rgb(245, 245, 245);
   text-align: center;
 }
 
+.alertTable {
+  color: rgb(245, 245, 245);
+}
+
+.card-text-filter {
+  text-align: right;
+}
+
 .create-alert {
-  color: white;
+  color: rgb(240, 100, 73);
 }
 </style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,28 +1,29 @@
-import Vue from 'vue'
-import App from './App.vue'
-import { BootstrapVue } from 'bootstrap-vue'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
+import Vue from "vue";
+import App from "./App.vue";
+import { BootstrapVue } from "bootstrap-vue";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
 
 /* import the fontawesome core */
-import { library } from '@fortawesome/fontawesome-svg-core'
+import { library } from "@fortawesome/fontawesome-svg-core";
 
 /* import font awesome icon component */
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
 /* import specific icons */
-import { faCirclePlus, faUserSecret } from '@fortawesome/free-solid-svg-icons'
+import { faCirclePlus, faUserSecret } from "@fortawesome/free-solid-svg-icons";
+
+// import { faCirclePlus, faUserSecret } from "@fortawesome/free-light-svg-icons";
 
 /* add icons to the library */
-library.add(faUserSecret, faCirclePlus)
+library.add(faUserSecret, faCirclePlus);
 
 /* add font awesome icon component */
-Vue.component('font-awesome-icon', FontAwesomeIcon)
+Vue.component("font-awesome-icon", FontAwesomeIcon);
 
-
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 Vue.use(BootstrapVue);
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
