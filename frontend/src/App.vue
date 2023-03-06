58c0d935abc27901da61dac3723bfd8e4d951280
שגיב ברקוביץ <u9256862@bsmch.net>
2023-03-06 13:06:37 +0200

    feat(header.vue): added header implementation

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,13 +1,16 @@
 <template>
   <div id="app">
+    <Header></Header>
   </div>
 </template>
 
 <script>
 import popUp from "./components/eventPopup.vue";
+import Header from "./components/header.vue";
 export default {
   components: {
     popUp,
+    Header
   },
 };
 </script>
