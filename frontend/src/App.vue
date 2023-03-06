b745095939bfb6245d9a2736ba2924a1a0613282
עמית בוסקילה <u9181071@bsmch.net>
2023-03-06 10:57:30 +0200

    resolve conflicts

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <div id="app">
     <popUp />
+    <eventMap />
   </div>
 </template>
 
 <script>
 import popUp from "./components/eventPopup.vue";
+import eventMap from "./components/Map.vue";
 
 export default {
   components: {
     popUp,
+    eventMap,
   },
 };
 </script>
