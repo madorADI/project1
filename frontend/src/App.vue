a843022a8ef2c957ade5c0adc9c6c7097a7041d3
עמית בוסקילה <u9181071@bsmch.net>
2023-03-06 09:38:54 +0200

    fix(eventPopup.vue): added modal component

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,13 +1,16 @@
 <template>
   <div id="app">
+    <popUp />
   </div>
 </template>
 
 <script>
-
+import popUp from "./components/eventPopup.vue";
 export default {
-
-}
+  components: {
+    popUp,
+  },
+};
 </script>
 
 <style></style>
