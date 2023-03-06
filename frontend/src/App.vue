31ca2ad7981ec7ae2358ef53bd03e6d612f3a6be
נויה באור <u9150719@bsmch.net>
2023-03-06 11:21:58 +0200

    feat(App.vue) removed router

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,14 +1,17 @@
 <template>
-  <div id="view">
-      <router-view />
-    </div>
+  <div id="app">
+    <Map></Map>
+    <LatestAlerts/>
+  </div>
 </template>
 
 <script>
-
+import Map from "../src/components/Map.vue";
+import LatestAlerts from "../src/components/LatestAlerts.vue";
 export default {
-
-}
+  name: "App",
+  components: { Map, LatestAlerts },
+};
 </script>
 
 <style></style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,8 +3,7 @@ import App from "./App.vue";
 import { BootstrapVue } from "bootstrap-vue";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
-import router from "./router";
-import VueRouter from "vue-router";
+
 
 /* import the fontawesome core */
 import { library } from "@fortawesome/fontawesome-svg-core";
@@ -24,10 +23,10 @@ library.add(faUserSecret, faCirclePlus);
 Vue.component("font-awesome-icon", FontAwesomeIcon);
 
 Vue.config.productionTip = false;
-Vue.use(VueRouter);
+
 
 Vue.use(BootstrapVue);
 new Vue({
-  router,
+
   render: (h) => h(App),
 }).$mount("#app");
