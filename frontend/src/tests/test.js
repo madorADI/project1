304928474336845483a5657860e1f404c07269c0
עמית בוסקילה <u9181071@bsmch.net>
2023-03-08 20:05:16 +0200

    d

---
+++ frontend/src/tests/test.js
@@ -0,0 +1,29 @@
+describe("formatDate", () => {
+  test("increments the current number by 1", () => {
+    expect(1).toBe(1);
+  });
+  test("increments the current number by 1", () => {
+    expect(1).toBe(1);
+  });
+  test("increments the current number by 1", () => {
+    expect(1).toBe(1);
+  });
+  test("increments the current number by 1", () => {
+    expect(1).toBe(1);
+  });
+});
+
+describe("formatDate", () => {
+  test("increments the current number by 1", () => {
+    expect(1).toBe(1);
+  });
+  test("increments the current number by 1", () => {
+    expect(1).toBe(1);
+  });
+  test("increments the current number by 1", () => {
+    expect(1).toBe(1);
+  });
+  test("increments the current number by 1", () => {
+    expect(1).toBe(1);
+  });
+});
