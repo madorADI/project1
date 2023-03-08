90d01efa931460e4ffc0a98c607b69d4d7ce1a3d
קרן בינדרמן <u9088830@bsmch.net>
2023-03-08 13:53:10 +0200

    connetc to  db server

--- backend/db/db.js
+++ backend/db/db.js
@@ -2,7 +2,6 @@ const { MongoClient } = require("mongodb");
 require("dotenv").config();
 
 const dbName = `${process.env["DB_NAME"]}`;
-// const mongodbUrl = `mongodb://${process.env["HOST_IP"]}:30350`;
 const mongodbUrl = `mongodb://mongo:${process.env["DB_USERNAME"]}@${process.env["HOST_IP"]}:${process.env["DB_PORT"]}/?authSource=admin`;
 
 let mongodb;
