bfdf6a89709f112385dfb65d8f67872329e07323
נויה באור <u9150719@bsmch.net>
2023-03-08 11:36:58 +0200

    feat(map) added icons by weapon

--- frontend/src/components/Map.vue
+++ frontend/src/components/Map.vue
@@ -1,15 +1,30 @@
 <template>
   <div style="height: 100vh; width: 100vw">
     <br />
-    <l-map :zoom="zoom" :center="center" :options="mapOptions" @click="selectBomb" style="height: 80%" class="map">
+    <l-map
+      :zoom="zoom"
+      :center="center"
+      :options="mapOptions"
+      @click="selectBomb"
+      style="height: 80%"
+      class="map"
+    >
       <l-tile-layer :url="url" />
       <!--change key, the markers should be from db-->
-      <l-marker v-for="marker in markers" :key="marker._id" :lat-lng="marker.coordinates"
-        @click="changeSelectedAlertId(marker._id)" :icon="bomb">
+      <l-marker
+        v-for="marker in markers"
+        :key="marker._id"
+        :lat-lng="marker.coordinates"
+        @click="changeSelectedAlertId(marker._id)"
+        :icon="getIconByName(marker.weapon).icon"
+      >
         <l-tooltip>{{ marker.description }}</l-tooltip>
       </l-marker>
-      <l-marker v-if="this.selectedLat && this.selectedLng" :lat-lng="[this.selectedLat, this.selectedLng]"
-        :icon="selected">
+      <l-marker
+        v-if="this.selectedLat && this.selectedLng"
+        :lat-lng="[this.selectedLat, this.selectedLng]"
+        :icon="selected"
+      >
         <l-tooltip>המיקום שנבחר</l-tooltip>
       </l-marker>
     </l-map>
@@ -40,13 +55,62 @@ export default {
       mapOptions: {
         zoomSnap: 0.5,
       },
-      // markers: [],
-      bomb: icon({
-        iconUrl:
-          "http://www.clker.com/cliparts/y/e/Q/T/p/N/red-bomb.svg.hi.png",
-        iconSize: [30, 37],
-        iconAnchor: [16, 37],
-      }),
+      icons: [
+        {
+          name: "אבנים",
+          icon: icon({
+            iconUrl:
+              "https://cdn-icons-png.flaticon.com/512/1213/1213131.png",
+            iconSize: [30, 37],
+            iconAnchor: [16, 37],
+          }),
+        },
+        {
+          name: "רכב",
+          icon: icon({
+            iconUrl:
+              "https://www.iconpacks.net/icons/2/free-car-icon-2897-thumb.png",
+            iconSize: [30, 37],
+            iconAnchor: [16, 37],
+          }),
+        },
+        {
+          name: "בקת''ב",
+          icon: icon({
+            iconUrl:
+              "https://cdn-icons-png.flaticon.com/512/238/238971.png",
+            iconSize: [30, 37],
+            iconAnchor: [16, 37],
+          }),
+        },
+        {
+          name: "סכין",
+          icon: icon({
+            iconUrl:
+              "https://cdn-icons-png.flaticon.com/512/2511/2511208.png",
+            iconSize: [30, 37],
+            iconAnchor: [16, 37],
+          }),
+        },
+        {
+          name: "נשק חם",
+          icon: icon({
+            iconUrl:
+              "https://www.iconpacks.net/icons/2/free-gun-icon-1950-thumb.png",
+            iconSize: [30, 37],
+            iconAnchor: [16, 37],
+          }),
+        },
+        {
+          name: "ארטילריה",
+          icon: icon({
+            iconUrl:
+              "https://cdn-icons-png.flaticon.com/512/238/238987.png",
+            iconSize: [30, 37],
+            iconAnchor: [16, 37],
+          }),
+        },
+      ],
       selected: icon({
         iconUrl:
           "https://upload.wikimedia.org/wikipedia/commons/thumb/0/04/Red_Arrow_Down.svg/1200px-Red_Arrow_Down.svg.png",
@@ -73,6 +137,9 @@ export default {
     selectBomb(event) {
       this.changeSelectedLat(event.latlng.lat);
       this.changeSelectedLng(event.latlng.lng);
+    },
+    getIconByName(name) {
+      return this.icons.find((elem) => elem.name === name);
     },
   },
 };
